0d26b76a3536de307699ffa950e080c89d695402
Hongxiong Xian (Wicresoft) <v-hoxian@microsoft.com>
2018-03-15 15:34:55 +0800

    Add a new test case against freebsd multi disks test.

--- remote-scripts/E2E-HOT-ADD-REMOVE-NEW-DATA-DISK-SEQ-RANDOM.ps1
+++ remote-scripts/E2E-HOT-ADD-REMOVE-NEW-DATA-DISK-SEQ-RANDOM.ps1
@@ -28,13 +28,36 @@ if ($isDeployed)
         #Also adds the VM name to the beginning of the file name 
         $DataDiskUri=$storageAcc.PrimaryEndpoints.Blob.ToString() + "vhds/" + $vmName 
 
+		$diskNumsBeforAddVHD = RunLinuxCmd -username $user -password $password -ip $hs1VIP -port $hs1vm1sshport -command "camcontrol devlist | wc -l" -runAsSudo
+		sleep 1
+		# $diskNumsBeforAddVHD = $diskNumsBeforAddVHD -replace '\D+(\d+)\D+','$1'
+		$diskNumsBeforAddVHD = $diskNumsBeforAddVHD -replace "[^0-9]" , ''
+		LogMsg "There are $diskNumsBeforAddVHD disks before adding new disks"
+		
         #Max data disks are 16 for A4
+		$maxDisks = 16
 		if( $currentTestData.TestType -eq "random" )
 		{
 			LogMsg "Add SCSI disks with random LUNs"
-			$length = 16
+			$length = [int]$maxDisks
 			$testLUNs = @()
 			for( $i = 0; $i -lt $length; $i++ )
+			{
+				do 
+				{
+					$random = Get-Random
+					$lun = $random % $length
+				} while ( $lun -in $testLUNs )
+				$testLUNs += $lun
+			}
+		}
+		elseif( $currentTestData.TestType -eq "randomBeginWithLun1" )
+		{
+			LogMsg "Add SCSI disks with random LUNs except LUN 0"
+			$length = [int]$maxDisks
+			$testLUNs = @()
+			$testLUNs += 0
+			for( $i = 0; $i -lt $length-1; $i++ )
 			{
 				do 
 				{
@@ -47,7 +70,8 @@ if ($isDeployed)
 		else
 		{
 			LogMsg "Add SCSI disks with sequential LUNs"
-			$testLUNs= 0..15
+			$lenth = [int]$maxDisks - 1
+			$testLUNs= 0..$lenth
 			$length = $testLUNs.Length
 		}
 
@@ -56,74 +80,88 @@ if ($isDeployed)
             $diskSize = (($newLUN + 1)*10)
             LogMsg "Adding disk ---- LUN: $newLUN   DiskSize: $diskSize"
             Add-AzureRmVMDataDisk -CreateOption empty -DiskSizeInGB $diskSize -Name $vmName-$newLUN -VhdUri $DataDiskUri-Data$newLUN.vhd -VM $vmdiskadd -Caching ReadWrite -lun $newLUN 
+			sleep 1
         }
         
         #Updates the VM with the disk config - does not require a reboot 
         Update-AzureRmVM -ResourceGroupName $rgNameOfVM -VM $vmdiskadd
-
-		RemoteCopy -uploadTo $hs1VIP -port $hs1vm1sshport -files $currentTestData.files -username $user -password $password -upload
-		RunLinuxCmd -username $user -password $password -ip $hs1VIP -port $hs1vm1sshport -command "chmod +x *" -runAsSudo
-		LogMsg "Executing : bash $($currentTestData.testScript) $length"
-		RunLinuxCmd -username $user -password $password -ip $hs1VIP -port $hs1vm1sshport -command "bash $($currentTestData.testScript) $length" -runAsSudo
-
-		RemoteCopy -download -downloadFrom $hs1VIP -files "/root/state.txt, /root/summary.log" -downloadTo $LogDir -port $hs1vm1sshport -username $user -password $password
-		$testResultOfAddDisks = Get-Content $LogDir\summary.log
-		$testStatus = Get-Content $LogDir\state.txt
-		LogMsg "Test result : $testResultOfAddDisks"
-        if ($testStatus -eq "TestCompleted")
+		LogMsg "Wait 60 seconds to update azure vm after adding new disks."
+        sleep 60
+        $diskNumsBeforRemoveVHD = RunLinuxCmd -username $user -password $password -ip $hs1VIP -port $hs1vm1sshport -command "camcontrol devlist | wc -l" -runAsSudo
+		sleep 1
+		$diskNumsBeforRemoveVHD = $diskNumsBeforRemoveVHD -replace "[^0-9]" , ''
+		LogMsg "There are $diskNumsBeforRemoveVHD disks after adding new disks"
+		
+		$diff = [int]($diskNumsBeforRemoveVHD - [int]$diskNumsBeforAddVHD )
+		if( $diff -ne [int]$maxDisks )
 		{
-			LogMsg "Add disks successfully"
-            $testResultOfAddDisks = "PASS"
+			$testResultOfAddDisks = "FAIL"
+			$testResult = "FAIL"
+			$total = [int]$diskNumsBeforAddVHD + [int]$maxDisks
+			LogMsg "Add new disk failed. It should be $total, but it's $diskNumsBeforRemoveVHD."
 		}
         else
-        {
-            LogMsg "Add the VHDs failed"
-            $testResultOfAddDisks = "FAIL"
-        }
-        
-        $temp = RunLinuxCmd -username $user -password $password -ip $hs1VIP -port $hs1vm1sshport -command "camcontrol devlist | wc -l" -runAsSudo
-        $temp = $temp.Split(" ")
-        $diskNumsBeforRemoveVHD = $temp[$temp.length - 1]
-		LogMsg "The sum of disks before removing: $diskNumsBeforRemoveVHD"
-		
-        #To delete the VHDs 
-        $testResultOfRemoveDisks = "PASS"
-        foreach ($newLUN in $testLUNs)
-        {
-            Remove-AzureRmVMDataDisk -VM $vmdiskadd -Name  $vmName-$newLUN
-            sleep 10
-            Update-AzureRmVM -ResourceGroupName $rgNameOfVM  -VM $vmdiskadd
-            
-            $temp = RunLinuxCmd -username $user -password $password -ip $hs1VIP -port $hs1vm1sshport -command "camcontrol devlist | wc -l" -runAsSudo
-			$temp = $temp.Split(" ")
-			$currentDiskNums = $temp[$temp.length - 1]
-			LogMsg "The current disks: $currentDiskNums"
-            $diffNum = [int]( $diskNumsBeforRemoveVHD - $currentDiskNums )
-            if( $diffNum -ne 1 )
-            {
-                $testResultOfRemoveDisks = "FAIL"
-                LogMsg "Remove the VHDs failed @LUN is $newLUN"
-            }
-            $diskNumsBeforRemoveVHD = $diskNumsBeforRemoveVHD - 1
-        
-        }
+		{
+			$testResultOfAddDisks = "PASS"
+			if( $currentTestData.writeDisk -eq "yes" )
+			{		
+				RemoteCopy -uploadTo $hs1VIP -port $hs1vm1sshport -files $currentTestData.files -username $user -password $password -upload
+				RunLinuxCmd -username $user -password $password -ip $hs1VIP -port $hs1vm1sshport -command "chmod +x *" -runAsSudo
+				LogMsg "Executing : bash $($currentTestData.testScript) $length"
+				RunLinuxCmd -username $user -password $password -ip $hs1VIP -port $hs1vm1sshport -command "bash $($currentTestData.testScript) $length" -runAsSudo
 
-        $temp = RunLinuxCmd -username $user -password $password -ip $hs1VIP -port $hs1vm1sshport -command "camcontrol devlist | wc -l" -runAsSudo
-		$temp = $temp.Split(" ")
-		$out = $temp[$temp.length - 1]
-		LogMsg "The current disks: $out"
-		if( $out -eq 3  -and $testResultOfAddDisks -eq "PASS" -and  $testResultOfRemoveDisks -eq "PASS" )
-        {
-            $testResult = "PASS"
-        }
-        else
-        {
-            LogMsg "Delete the VHDs failed"
-            $testResult = "FAIL"
-        }
-		
+				RemoteCopy -download -downloadFrom $hs1VIP -files "/root/state.txt, /root/summary.log" -downloadTo $LogDir -port $hs1vm1sshport -username $user -password $password
+				$testStatus = Get-Content $LogDir\state.txt
+				if ($testStatus -eq "TestCompleted")
+				{
+					LogMsg "Write disks successfully"
+					$testResultOfAddDisks = "PASS"
+				}
+				else
+				{
+					LogMsg "Write disks failed"
+					$testResultOfAddDisks = "FAIL"
+				}			
+			}
+		}
+				
+		if( $testResultOfAddDisks -eq "PASS" )
+		{
+			#To delete the VHDs 
+			$testResultOfRemoveDisks = "PASS"
+			foreach ($newLUN in $testLUNs)
+			{
+				Remove-AzureRmVMDataDisk -VM $vmdiskadd -Name  $vmName-$newLUN
+				sleep 10
+				Update-AzureRmVM -ResourceGroupName $rgNameOfVM  -VM $vmdiskadd
+				
+				$currentDiskNums = RunLinuxCmd -username $user -password $password -ip $hs1VIP -port $hs1vm1sshport -command "camcontrol devlist | wc -l" -runAsSudo
+				$currentDiskNums = $currentDiskNums -replace "[^0-9]" , ''
+				$diffNum =  [int]$diskNumsBeforRemoveVHD - [int]$currentDiskNums
+				if( $diffNum -ne 1 )
+				{
+					$testResultOfRemoveDisks = "FAIL"
+					LogMsg "Remove the VHDs failed @LUN is $newLUN"
+					LogMsg "diskNumsBeforRemoveVHD: $diskNumsBeforRemoveVHD   "
+					LogMsg "currentDiskNums: $currentDiskNums   "
+				}
+				$diskNumsBeforRemoveVHD = [int]$diskNumsBeforRemoveVHD - 1
+
+			}
+
+			$out = RunLinuxCmd -username $user -password $password -ip $hs1VIP -port $hs1vm1sshport -command "camcontrol devlist | wc -l" -runAsSudo
+			$out = $out -replace "[^0-9]" , ''
+			if( $out -eq $diskNumsBeforAddVHD  -and  $testResultOfRemoveDisks -eq "PASS" )
+			{
+				$testResult = "PASS"
+			}
+			else
+			{
+				LogMsg "Delete the VHDs failed"
+				$testResult = "FAIL"
+			} 		
+		}       		
 	}
-
 	catch
 	{
 		$ErrorMessage =  $_.Exception.Message
